51f95921d771cfde06c407da749b3173c274578e
James Reddick <reddicj@gmail.com>
2021-09-27 11:03:42 +0100

    Update stuff

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -2,14 +2,16 @@ import sbt._
 
 object Libs {
 
+  val ZIO = "1.0.12"
+
   val libraryDependencies = Seq(
     "org.apache.commons" % "commons-lang3" % "3.12.0",
     "org.apache.commons" % "commons-text" % "1.9",
     "commons-io" % "commons-io" % "2.11.0",
     "org.typelevel" %% "cats-core" % "2.6.1",
-    "dev.zio" %% "zio" % "1.0.11",
-    "dev.zio" %% "zio-test" % "1.0.11" % Test,
-    "dev.zio" %% "zio-test-sbt" % "1.0.11" % Test,
+    "dev.zio" %% "zio" % ZIO,
+    "dev.zio" %% "zio-test" % ZIO % Test,
+    "dev.zio" %% "zio-test-sbt" % ZIO % Test,
     "dev.zio" %% "zio-interop-cats" % "3.1.1.0",
     "com.softwaremill.quicklens" %% "quicklens" % "1.7.4"
   )
