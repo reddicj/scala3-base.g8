6dcaf9a9268875a7bac7b5492d87a178fa8e99b9
James Reddick <reddicj@gmail.com>
2023-04-29 13:02:44 +0100

    Update libs

--- src/main/g8/project/Libs.scala
+++ src/main/g8/project/Libs.scala
@@ -3,14 +3,14 @@ import Keys.*
 
 object Libs {
 
-  val ZIO = "2.0.5"
+  val ZIO = "2.0.13"
 
   val dependencies = libraryDependencies ++= Seq(
     "org.typelevel"              %% "cats-core"    % "2.9.0",
     "dev.zio"                    %% "zio"          % ZIO,
     "dev.zio"                    %% "zio-test"     % ZIO % "test,it",
     "dev.zio"                    %% "zio-test-sbt" % ZIO % "test,it",
-    "dev.zio"                    %% "zio-prelude"  % "1.0.0-RC16",
-    "com.softwaremill.quicklens" %% "quicklens"    % "1.9.0"
+    "dev.zio"                    %% "zio-prelude"  % "1.0.0-RC19",
+    "com.softwaremill.quicklens" %% "quicklens"    % "1.9.2"
   )
 }
